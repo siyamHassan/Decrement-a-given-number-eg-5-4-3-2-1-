feb6d2e4a3fac7e9f672a58c488d71142e1ec2c2
Siam Hasan <ratkim158@gmail.com>
2025-10-14 19:02:03 -0700

    Add files via upload

---
+++ reverse.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    cout<<"Enter a number:";
+    cin>>x;
+    for(int i = x;i>=1;i--){
+        cout<<i<<endl;
+    }
+
+    return 0;
+}
